87431e5b08576f4ebf88d77b6e428ffe8b2cc9d0
Soummya Mallick <mallicksm@gmail.com>
2021-11-09 11:30:43 -0800

    cleanup and back to nvim

--- nvim.sh
+++ nvim.sh
@@ -2,13 +2,12 @@
 #-------------------------------------------------------------------------------
 set -eo pipefail
 
-git_repo_local=$HOME/git_repo/svim
-git_repo_remote=https://github.com/mallicksm/svim.git
+git_repo_local=$HOME/git_repo/nvim
+git_repo_remote=https://github.com/mallicksm/nvim.git
 git_repo=$git_repo_local
 
 function main() {
    print_logo
- 
    rm -rf $HOME/.config/nvim
    rm -rf $HOME/.local/share/nvim
    git clone $git_repo $HOME/.config/nvim
