fefd4aa5ffb702a2222df2c28fd7509f991bad66
Soummya Mallick <mallicksm@gmail.com>
2021-11-09 10:50:53 -0800

    went back

--- init.lua
+++ init.lua
@@ -14,19 +14,15 @@ Maintainer: Brainf+ck
 Website: https://github.com/brainfucksec/neovim-lua
 
 --]]
-local fn = vim.fn
------------------------------------------------------------
--- Fix rtp
------------------------------------------------------------
-vim.opt.rtp:prepend('$HOME/.config/svim')
-vim.opt.rtp:prepend('$HOME/.local/share/svim/git-svim')
-vim.opt.rtp:prepend('$HOME/.local/share/svim')
 
 -----------------------------------------------------------
 -- Import Lua modules
 -----------------------------------------------------------
+
+local fn = vim.fn
+
 -- Auto install packer.nvim
-local install_path = '$HOME/.local/share/svim/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.isdirectory(install_path) == 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
