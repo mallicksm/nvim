946f1aeba9d13f72996071fd4373c7d39ca02931
Soummya Mallick <Soummya.Mallick@microchip.com>
2021-12-03 03:08:56 -0800

    cleanup

--- lua/config/nerdtree.lua
+++ lua/config/nerdtree.lua
@@ -2,18 +2,14 @@
 local g = vim.g
 local kmap = require('utils').kmap
 
-g.NERDTreeDelimiter = "\t"
 g.NERDTreeWinSize = 35
-
-g.NERDTreeIgnore = {'.svn$', '.git$', '.d$', '.o$'}
 g.NERDTreeBookmarksSort = true
 g.NERDTreeShowBookmarks = true
 g.NERDTreeShowLineNumbers = true
 g.NERDTreeMinimalUI = true
-g.NERDTreeDirArrows = 0
-g.WebDevIconsUnicodeDecorateFolderNodes = 1
-g.DevIconsEnableFoldersOpenClose = 1
-g.DevIconsEnableFolderExtensionPatternMatching = 1
-
-vim.cmd [[nnoremap <expr> <leader>e g:NERDTree.IsOpen() ? ':NERDTreeClose<CR>' : @% == '' ? ':NERDTree<CR>' : ':NERDTreeFind<CR>']]
+g.NERDTreeSortOrder = {'/$', '*', '[[timestamp]]'}
+g.DevIconsEnableFoldersOpenClose = true
+vim.cmd [[
+nnoremap <expr> <leader>e g:NERDTree.IsOpen() ? ':NERDTreeClose<CR>' : @% == '' ? ':NERDTree<CR>' : ':NERDTreeFind<CR>'
+]]
 kmap('n', '<leader>f', '<CMD>NERDTreeFind<CR>')
