10f9369399dc1c8cebefbe63e23da976b7440b60
Soummya Mallick <Soummya.Mallick@microchip.com>
2022-01-02 07:46:24 -0800

    added lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -74,14 +74,11 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
---[[
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-
---]]
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
