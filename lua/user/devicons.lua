fd2a3907309549858c5c8292acd8f11b1e39c5d7
Soummya Mallick <Soummya.Mallick@microchip.com>
2022-04-25 12:52:06 -0700

    treesitter improvements

--- lua/user/devicons.lua
+++ lua/user/devicons.lua
@@ -1,4 +1,3 @@
-
 local status_ok, nvim_devicons = pcall(require, "nvim-web-devicons")
 if not status_ok then
    return
@@ -7,5 +6,5 @@ end
 nvim_devicons.setup({
    default = true,
 })
-nvim_devicons.get_icon("*",".S",{default = true})
+nvim_devicons.get_icon("*", ".S", { default = true })
 nvim_devicons.set_default_icon('', '#6d8086')
--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,3 +1,4 @@
+print(vim.fn.stdpath("data"))
 return {
    settings = {
 
@@ -9,6 +10,11 @@ return {
             library = {
                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                [vim.fn.stdpath("config") .. "/lua"] = true,
+               [vim.fn.stdpath("config") .. "/lua/user"] = true,
+               [vim.fn.stdpath("config") .. "/lua/user/lsp"] = true,
+               [vim.fn.stdpath("config") .. "/lua/user/lsp/settings"] = true,
+               [vim.fn.stdpath("data") .. "/site/pack/packer/start"] = true,
+               [vim.fn.stdpath("data") .. "/site/pack/packer/opt"] = true,
             },
          },
       },
